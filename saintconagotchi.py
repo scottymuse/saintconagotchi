42ee2d1f03de69a2c20d13d7e70da37211743ab6
scottymuse <scottymuse@gmail.com>
2019-11-16 00:29:57 -0700

    Added a lot of stuff. Mostly the Auto/manual mode toggle.

--- saintconagotchi.py
+++ saintconagotchi.py
@@ -2,8 +2,11 @@
 
 import pygame
 import pyinotify
+import psutil
+from subprocess import PIPE
+from time import time
 
-screen = pygame.display.set_mode((640,312))
+screen = pygame.display.set_mode((640,480))
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CLOSE_WRITE(self, event):
@@ -12,13 +15,45 @@ class EventHandler(pyinotify.ProcessEvent):
         screen.blit(img, (0,0))
         pygame.display.flip()
 
+def events(self, event):
+    if event.type == pygame.KEYDOWN:
+        if event.key == K_ESCAPE:
+            # Toggle Manual/Auto Mode
+            #touch /root/.pwnagotchi-auto && systemctl restart pwnagotchi
+            # Check if currently in manual mode
+            ps = None
+            to_auto = False
+            for p in psutil.process_iter():
+                if p.name = "pwnagotchi":
+                    ps = p.pid
+            if ps is not None:
+                proc = psutil.Process(ps)
+                if time() - proc.create_time() < 30:
+                    #Give it a moment!!!
+                    return
+                cmd = proc.cmdline()
+                if "--manual" in cmd:
+                    to_auto = True
+            if to_auto:
+                open('/root/.pwnagotchi-auto')
+            psutil.Popen(["/bin/systemctl", "restart", "pwnagotchi"])
+
+        if event.key == K_r:
+            # Toggle LED activity notifications
+        if event.key == K_l:
+            # Toggle mood LED
+
 def main():
     pygame.init()
+    
     wm = pyinotify.WatchManager()
     mask = pyinotify.IN_CLOSE_WRITE
-    handler = EventHandler()
-    notifier = pyinotify.Notifier(wm, handler)
+    notifier = pyinotify.ThreadedNotifier(wm, EventHandler())
+    notifier.start()
     wdd = wm.add_watch('/root/pwnagotchi.png', mask)
-    notifier.loop()
+
+    for event in pygame.event.get():
+        events(event)
+
 
 main()
