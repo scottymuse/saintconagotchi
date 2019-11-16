d1f7623d277ade72e350a99e958e6a38a3f9cf17
scottymuse <scottymuse@gmail.com>
2019-11-16 00:31:19 -0700

    Typeo

--- saintconagotchi.py
+++ saintconagotchi.py
@@ -24,7 +24,7 @@ def events(self, event):
             ps = None
             to_auto = False
             for p in psutil.process_iter():
-                if p.name = "pwnagotchi":
+                if p.name == "pwnagotchi":
                     ps = p.pid
             if ps is not None:
                 proc = psutil.Process(ps)
