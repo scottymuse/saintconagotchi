d5a1bd9a2e2aa4748a242b53a3e5d28ecd40ef4d
scottymuse <scottymuse@gmail.com>
2019-11-19 01:08:08 -0700

    Added some stuff. Desperately needs help.

--- saintconagotchi.py
+++ saintconagotchi.py
@@ -6,24 +6,41 @@ import pyinotify
 import psutil
 from subprocess import PIPE
 from time import time
+import Queue`
+import thread
 import board
 import neopixel
 
 screen = pygame.display.set_mode((640,480))
 strip = neopixel.Neopixel(pin=board.D18, n=2)
 
+# Pwnagotchi logfile generator
+def pwnagotchi_logfile_reader(logfile):
+    logfile.seek(0.2)
+    while True:
+        line = logfile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+logfile = open("/var/log/pwnagotchi.log")
+loglines = pwnagotchi_logfile_reader(logfile)
+
+def update_iamge():
+    img = pygame.image.load('/root/pwnagotchi.png')
+    img = pygame.transform.scale(img, (640,312))
+    screen.blit(img, (0,0))
+    pygame.display.flip()
+
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CLOSE_WRITE(self, event):
-        img = pygame.image.load('/root/pwnagotchi.png')
-        img = pygame.transform.scale(img, (640,312))
-        screen.blit(img, (0,0))
-        pygame.display.flip()
+        update_image()
 
 def events(event):
     if event.type == pygame.KEYDOWN:
         if event.key == K_ESCAPE:
             # Toggle Manual/Auto Mode
-            #touch /root/.pwnagotchi-auto && systemctl restart pwnagotchi
             # Check if currently in manual mode
             ps = None
             to_auto = False
@@ -38,7 +55,7 @@ def events(event):
                 cmd = proc.cmdline()
                 if "--manual" in cmd:
                     to_auto = True
-            if to_auto:
+            if to_auto: # Currently in manual, create this file to make it auto
                 open('/root/.pwnagotchi-auto')
             psutil.Popen(["/bin/systemctl", "restart", "pwnagotchi"])
 
@@ -47,18 +64,23 @@ def events(event):
         #if event.key == K_l:
             # Toggle mood LED
 
-def main():
-    pygame.init()
-    
-    wm = pyinotify.WatchManager()
-    mask = pyinotify.IN_CLOSE_WRITE
-    notifier = pyinotify.ThreadedNotifier(wm, EventHandler())
-    notifier.start()
-    wdd = wm.add_watch('/root/pwnagotchi.png', mask)
+pygame.init()
 
-    while True:
-        for event in pygame.event.get():
-            events(event)
+update_image() # Load the first image
+
+# Thread to watch the image for changes and update
+wm = pyinotify.WatchManager()
+mask = pyinotify.IN_CLOSE_WRITE
+notifier = pyinotify.ThreadedNotifier(wm, EventHandler())
+notifier.start()
+wdd = wm.add_watch('/root/pwnagotchi.png', mask)
+
+# Threads to read logfile and update LEDs
+# goes here
+
+#Loop to check for button presses
+while True:
+    for event in pygame.event.get():
+        events(event)
 
 
-main()
